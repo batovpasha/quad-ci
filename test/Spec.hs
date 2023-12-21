4a5e8403beb4b67af6069c6feba6cb18ff0c9ac9
Pavlo Batov <batovpasha@gmail.com>
2023-12-21 21:00:15 +0200

    feat: add core domain model

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,48 @@
+import           Core
+import           Prelude
 import           RIO
+import           Test.Hspec
+
+-- Helper functions
+makeStep :: Text -> Text -> [Text] -> Step
+makeStep name image commands =
+  Step {name = StepName name, image = Image image, commands = commands}
+
+makePipeline :: [Step] -> Pipeline
+makePipeline steps = Pipeline {steps = steps}
+
+-- Test values
+testPipeline :: Pipeline
+testPipeline =
+  makePipeline
+    [ makeStep "First step" "ubuntu" ["date"]
+    , makeStep "Second step" "ubuntu" ["uname -r"]
+    ]
+
+testBuild :: Build
+testBuild = Build {pipeline = testPipeline, state = BuildReady}
 
 main :: IO ()
-main = return ()
+main =
+  hspec $ do
+    describe "testBuild" $ do
+      it "should be a valid build" $ do
+        testBuild ==
+          Build
+            { pipeline =
+                Pipeline
+                  { steps =
+                      [ Step
+                          { name = StepName "First step"
+                          , commands = ["date"]
+                          , image = Image "ubuntu"
+                          }
+                      , Step
+                          { name = StepName "Second step"
+                          , commands = ["uname -r"]
+                          , image = Image "ubuntu"
+                          }
+                      ]
+                  }
+            , state = BuildReady
+            }
