2a359800463a5bd4a59813c788133933ab1ce3cf
Pavlo Batov <batovpasha@gmail.com>
2023-12-27 14:20:26 +0200

    test: add test that checks build for fail

--- test/Spec.hs
+++ test/Spec.hs
@@ -38,10 +38,21 @@ testRunSuccess runner = do
   result.state `shouldBe` BuildFinished BuildSucceeded
   Map.elems result.completedSteps `shouldBe` [StepSucceeded, StepSucceeded]
 
+testRunFailure :: Runner.Service -> IO ()
+testRunFailure runner = do
+  build <-
+    runner.prepareBuild $
+    makePipeline [makeStep "Should fail" "ubuntu" ["exit 1"]]
+  result <- runner.runBuild build
+  result.state `shouldBe` BuildFinished BuildFailed
+  Map.elems result.completedSteps `shouldBe`
+    [StepFailed (Docker.ContainerExitCode 1)]
+
 main :: IO ()
 main =
   hspec $ do
-    let dockerService = Docker.createService
-    let runnerService = Runner.createService dockerService
+    let docker = Docker.createService
+    let runner = Runner.createService docker
     beforeAll cleanupDocker $ describe "Quad CI" $ do
-      it "should run a build (success)" do testRunSuccess runnerService
+      it "should run a build (success)" do testRunSuccess runner
+      it "should run a build (failure)" do testRunFailure runner
