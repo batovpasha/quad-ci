13400949d2288c89c8bae28b95b6ad7c529640b7
Pavlo Batov <batovpasha@gmail.com>
2023-12-27 13:24:39 +0200

    test: add basic test that checks build run

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,8 +2,16 @@ import           Core
 import           Docker
 import           Prelude
 import           RIO
+import qualified RIO.Map              as Map
 import qualified RIO.NonEmpty.Partial as NonEmpty.Partial
+import qualified System.Process.Typed as Process
 import           Test.Hspec
+
+cleanupDocker :: IO ()
+cleanupDocker =
+  void $ do
+    Process.readProcessStdout
+      "docker rm -f $(docker ps -aq --filter \"label=quad\")"
 
 -- Helper functions
 makeStep :: Text -> Text -> [Text] -> Step
@@ -29,29 +37,24 @@ testBuild :: Build
 testBuild =
   Build {pipeline = testPipeline, state = BuildReady, completedSteps = mempty}
 
+runBuild :: Docker.Service -> Build -> IO Build
+runBuild docker build = do
+  newBuild <- Core.progress docker build
+  case newBuild.state of
+    BuildFinished _ -> pure newBuild
+    _ -> do
+      threadDelay (1 * 1000 * 1000)
+      runBuild docker newBuild
+
+testRunSuccess :: Docker.Service -> IO ()
+testRunSuccess docker = do
+  result <- runBuild docker testBuild
+  result.state `shouldBe` BuildFinished BuildSucceeded
+  Map.elems result.completedSteps `shouldBe` [StepSucceeded, StepSucceeded]
+
 main :: IO ()
 main =
   hspec $ do
-    describe "testBuild" $ do
-      it "should be a valid build" $ do
-        testBuild ==
-          Build
-            { pipeline =
-                Pipeline
-                  { steps =
-                      NonEmpty.Partial.fromList
-                        [ Step
-                            { name = StepName "First step"
-                            , commands = NonEmpty.Partial.fromList ["date"]
-                            , image = Docker.Image "ubuntu"
-                            }
-                        , Step
-                            { name = StepName "Second step"
-                            , commands = NonEmpty.Partial.fromList ["uname -r"]
-                            , image = Docker.Image "ubuntu"
-                            }
-                        ]
-                  }
-            , state = BuildReady
-            , completedSteps = mempty
-            }
+    let dockerService = createService
+    beforeAll cleanupDocker $ describe "Quad CI" $ do
+      it "should run a build (success)" do testRunSuccess dockerService
