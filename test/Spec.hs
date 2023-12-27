9031bdef0947d56349f8b32250ab9bcfe2f62590
Pavlo Batov <batovpasha@gmail.com>
2023-12-27 14:12:57 +0200

    refactor: move build-related functions to the separate Runner module

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,6 +4,7 @@ import           Prelude
 import           RIO
 import qualified RIO.Map              as Map
 import qualified RIO.NonEmpty.Partial as NonEmpty.Partial
+import qualified Runner
 import qualified System.Process.Typed as Process
 import           Test.Hspec
 
@@ -25,36 +26,22 @@ makeStep name image commands =
 makePipeline :: [Step] -> Pipeline
 makePipeline steps = Pipeline {steps = NonEmpty.Partial.fromList steps}
 
--- Test values
-testPipeline :: Pipeline
-testPipeline =
-  makePipeline
-    [ makeStep "First step" "ubuntu" ["date"]
-    , makeStep "Second step" "ubuntu" ["uname -r"]
-    ]
-
-testBuild :: Build
-testBuild =
-  Build {pipeline = testPipeline, state = BuildReady, completedSteps = mempty}
-
-runBuild :: Docker.Service -> Build -> IO Build
-runBuild docker build = do
-  newBuild <- Core.progress docker build
-  case newBuild.state of
-    BuildFinished _ -> pure newBuild
-    _ -> do
-      threadDelay (1 * 1000 * 1000)
-      runBuild docker newBuild
-
-testRunSuccess :: Docker.Service -> IO ()
-testRunSuccess docker = do
-  result <- runBuild docker testBuild
+testRunSuccess :: Runner.Service -> IO ()
+testRunSuccess runner = do
+  build <-
+    runner.prepareBuild $
+    makePipeline
+      [ makeStep "First step" "ubuntu" ["date"]
+      , makeStep "Second step" "ubuntu" ["uname -r"]
+      ]
+  result <- runner.runBuild build
   result.state `shouldBe` BuildFinished BuildSucceeded
   Map.elems result.completedSteps `shouldBe` [StepSucceeded, StepSucceeded]
 
 main :: IO ()
 main =
   hspec $ do
-    let dockerService = createService
+    let dockerService = Docker.createService
+    let runnerService = Runner.createService dockerService
     beforeAll cleanupDocker $ describe "Quad CI" $ do
-      it "should run a build (success)" do testRunSuccess dockerService
+      it "should run a build (success)" do testRunSuccess runnerService
