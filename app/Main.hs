fe5efac55e18d4584e333262550c5560a10e62bf
Pavlo Batov <batovpasha@gmail.com>
2024-03-04 18:44:59 +0200

    feat: implement cli module for the app

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,22 @@
-module Main
-  ( main
-  ) where
-
-import qualified Docker
+module Main where
+import qualified Cli
 import           RIO
+import qualified System.Log.Formatter      as Logger
+import qualified System.Log.Handler        as Handler
+import qualified System.Log.Handler.Simple as Logger
+import qualified System.Log.Logger         as Logger
+
+initLogger :: IO ()
+initLogger = do
+  logger <- Logger.getRootLogger
+  handler <- Logger.streamHandler stdout Logger.INFO
+  let formatter = Logger.simpleLogFormatter "[$time : $loggername : $prio] $msg"
+  Logger.saveGlobalLogger $
+    logger
+      & Logger.setHandlers [Handler.setFormatter handler formatter]
+      & Logger.setLevel Logger.INFO
 
 main :: IO ()
 main = do
-  dockerService <- Docker.createService
-  volume <- dockerService.createVolume
-  cId <-
-    dockerService.createContainer $
-    Docker.CreateContainerOptions
-      { image = (Docker.Image {name = "ubuntu", tag = "latest"})
-      , script = "echo 'ok'"
-      , volume = volume
-      }
-  dockerService.startContainer cId
-  dockerService.containerStatus cId
-  return ()
+  initLogger
+  Cli.main
